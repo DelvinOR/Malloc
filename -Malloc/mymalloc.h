40be72909b6cf5b0ecfb5f7c873e17ef52b625cc
Delvin Ortiz <delvin732@gmail.com>
2019-10-05 15:22:12 -0400

    Created mymalloc.h, mymalloc.c

---
+++ -Malloc/mymalloc.h
@@ -0,0 +1,8 @@
+#ifndef mymalloc_h
+#define mymalloc_h
+#define malloc(x) mymalloc(x, __FILE__, __LINE__)
+#define free(x) myfree(x, __FILE__, __LINE__)
+void* mymalloc();
+void myfree();
+static char myblock[4996];
+#endif
