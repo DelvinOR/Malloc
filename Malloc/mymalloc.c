98e26014e91648d1b3fda965dad34d335660276e
Delvin Ortiz <delvin732@gmail.com>
2019-10-05 20:33:23 -0400

    Switched to using structs and array

--- Malloc/mymalloc.c
+++ Malloc/mymalloc.c
@@ -3,39 +3,37 @@
 
 #define ptrsize sizeof(void*)
 
+struct metadata{
+    int size;
+    char inUse;
+    struct metadata * userData;
+};
+
 int main(int argc, char*argv){
     char* array = myblock;
     int * ptr = (int*) malloc(4);
-    printf("the data of this pointer is %d\n", ptr[0]);
+
     return 0;
 }
 void* mymalloc(int size){
-    char * blockPtr;
-    // Check that there is metadata in our myblock 
+
     if(myblock[0] == 0xFF && myblock[1] == 0x55){
         // Our static byte array has metadata in it
 
     }else{// There is data in our myblock
             myblock[0] = 0xFF;
             myblock[1] = 0x55; 
-            myblock[2] = 4096 - 2 - 1 - 4 - ptrsize;
-            myblock[6] = 'n';
-            blockPtr = &myblock[7];
-            *blockPtr = NULL;
-        
-        if(size <= myblock[2]){
-            myblock[2] = size;
-            myblock[6] = 'y';
-            myblock[7] = blockPtr;
-
-            myblock[7 + ptrsize + size] = myblock[2] - size;
-            myblock[7 + ptrsize + size + 1] = 'n';
             
-        }else{
-            printf("%s called this malloc method and %d is the line\n", __FILE__, __LINE__);
-            return NULL;
-        }
-        
+            struct metadata startBlock = {4096 - 2 - 1 - ptrsize, 'n', NULL};
+
+            struct metadata* ptr = &myblock[2];
+            
+
+            if(size > startBlock.size){
+                // Error
+            }else{
+
+            }     
     }
 }
 
@@ -48,3 +46,31 @@ void myfree(void * ptr){
 
 }
 
+/** Previous mymalloc code 
+ * void * blockPtr;
+    // Check that there is metadata in our myblock 
+    if(myblock[0] == 0xFF && myblock[1] == 0x55){
+        // Our static byte array has metadata in it
+
+    }else{// There is data in our myblock
+            myblock[0] = 0xFF;
+            myblock[1] = 0x55; 
+            int * ptr =(int*) &myblock[2];
+            *ptr = 4096 - 2 - 1 - 4 - ptrsize;
+            myblock[6] = 'n';
+            blockPtr = &myblock[7];
+        
+        if(size <= ptr[0]){
+            myblock[2] = size;
+            myblock[6] = 'y';
+
+            myblock[7 + ptrsize + size] = myblock[2] - size;
+            myblock[7 + ptrsize + size + 1] = 'n';
+            
+        }else{
+            printf("%s called this malloc method and %d is the line\n", __FILE__, __LINE__);
+            return (void*)blockPtr;
+        }
+        
+    }
+ * **/
